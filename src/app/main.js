89119b44b12313610efbb869482bb6051f3af668
Aenohe <fabien@gavory.fr>
2017-07-25 16:32:16 +0200

    wip

---
+++ src/app/main.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import { render } from 'react-dom'
+
+class Test extends Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+render(
+  <Test />
+);
