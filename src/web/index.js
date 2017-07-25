67acd33b553c680740346c04c95e81f8c36ebde7
Aenohe <fabien@gavory.fr>
2017-07-25 18:47:21 +0200

    wip

--- src/web/index.js
+++ src/web/index.js
@@ -4,11 +4,8 @@ const demo = require('./routes/demo');
 const path = require('path');
 
 const router = express.Router();
-const assetsPath = path.resolve(__dirname, '../build/assets');
 
 router.use('/', home);
 router.use('/demo', demo);
-
-router.use('/assets', express.static(assetsPath));
 
 module.exports = router;
