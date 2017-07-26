b65ff75681078d1d2a8bd6dc13bd770bf68a66ed
cavrois cedric <cavrois.cedric@hotmail.fr>
2017-07-26 12:29:00 +0200

    Add redux

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,56 @@
-import { Component } from 'react';
-import Test from './components/test';
+import React, { Component } from 'react'
+import { loadDemo } from './action-creators/DemoActionCreators'
+import { connect } from 'react-redux'
 
-const App = () => (
-  <Test />
-);
 
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    const { dispatch } = this.props
+    dispatch(loadDemo())
+  }
+
+  renderMenuBar() {
+    const { menuItems } = this.props
+
+    if (!menuItems || !menuItems.length) return null
+
+    return menuItems.map(item => (
+      <div className="menu-item">
+        item.label
+      </div>
+    ))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="menu-container">
+          <div className="menu">
+            { this.renderMenuBar() }
+          </div>
+        </div>
+        <h1>Hello there</h1>
+        { this.props.child }
+      </div>
+    );
+  }
+}
+
+
+
+const mapStateToProps = state => ({
+  menuItems: state.demo.demo
+})
+
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+
+  dispatch
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/app/main.js
+++ src/app/main.js
@@ -1,17 +1,43 @@
-import App from './app';
-import { render } from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { IndexRoute, Route, Router, Redirect, browserHistory } from 'react-router'
 import { AppContainer } from 'react-hot-loader';
 
-const appRender = Component => {
-  render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('app')
-  );
-};
 
-appRender(App);
+import reducers from './reducers/index'
+
+import App from './App'
+import Test from './components/Test'
+
+const appDomEl = document.getElementById('app')
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+const history = syncHistoryWithStore(browserHistory, store)
+
+if (appDomEl) {
+  const Root = ({ store }) => (
+    <AppContainer>
+      <Provider store={store}>
+        <Router history={history}>
+          <Route path="/" component={App}>
+            <Route path="/test" components={{child: Test}}/>
+          </Route>
+        </Router>
+      </Provider>
+    </AppContainer>
+  )
+
+  ReactDOM.render(
+    <Root store={store} />,
+    appDomEl
+  )
+}
 
 if (module.hot) {
   console.log('hot')
